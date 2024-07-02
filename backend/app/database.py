cdbcc1db7aae267688c11a0c9ce4c99c5234a37b
luis-ntonio <luis.gutierrez.g@utec.edu.pe>
2024-07-01 23:45:53 -0500

    adding .env

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,11 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
 
 db = SQLAlchemy()
 
+load_dotenv()
 
 def init_db(app):
     app.config["SQLALCHEMY_DATABASE_URI"] = (
-        "postgresql://postgres:Avatar2741088@localhost:5432/chess_db"  # Cambia esto con tus credenciales y nombre de la base de datos
+          os.getenv("SQLALCHEMY_DATABASE_URI")# Cambia esto con tus credenciales y nombre de la base de datos
     )
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
